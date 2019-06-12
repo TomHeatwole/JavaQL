e715223e551d1d29e1e8eb95b76d8d35fb6a7ee5
tomheatwole <tgh8xn@virginia.edu>
2019-06-12 03:24:56 -0400

    Add example of first successful unit test

--- UnitTests.cc
+++ UnitTests.cc
@@ -7,29 +7,53 @@
 #include <sys/wait.h>
 using namespace std;
 
+int num_passed = 0;
+int num_failed = 0;
+int read_fd;
+int write_fd;
+bool pass = true;
+
 bool setup_error(string message) {
     cout << "Failed to begin testing: " << message << endl;
     return 1;
 }
 
 // From stackoverflow: https://stackoverflow.com/questions/1583353/how-to-read-exactly-one-line
-bool read_line(int fd, string* line) {
+string read_line() {
+    if (!pass) return "";
+    string* line;
     static string buffer;
     string::iterator pos;
     while ((pos = find (buffer.begin(), buffer.end(), '\n')) == buffer.end()) {
         char buf [1025];
-        int n = read(fd, buf, 1024);
+        int n = read(read_fd, buf, 1024);
         if (n == -1) {
           *line = buffer;
           buffer = "";
-          return false;
+          return "failed to read line";
         }
         buf [n] = 0;
         buffer += buf;
     }
     *line = string(buffer.begin(), pos);
     buffer = string(pos + 1, buffer.end());
-    return true;
+    return *line;
+}
+
+void next_test(string name) {
+    if (pass) {
+        cout << "PASSED" << endl;
+        num_passed++;
+    } else {
+        cout << "FAILED" << endl;
+        pass = true;
+        num_failed++;
+    }
+    if (name != "done") cout << name << " . . . ";
+}
+
+void assert_true(bool exp) {
+    pass = pass && exp;
 }
 
 int main() {
@@ -49,18 +73,21 @@ int main() {
         strcpy(buff, name.c_str());
         char* argv[] = {buff};
         execv("t", argv);
-    } else {
-        close(child_to_parent[1]);
-        string line;
-        read_line(child_to_parent[0], &line);
-        cout << line << "\n";
-        read_line(child_to_parent[0], &line);
-        cout << line << "\n";
-        read_line(child_to_parent[0], &line);
-        cout << line << "\n";
-        read_line(child_to_parent[0], &line);
-        cout << line << "\n";
     }
+    close(child_to_parent[1]);
+    read_fd = child_to_parent[0];
+    write_fd = parent_to_child[1];
+    string line;
+    cout << "Connect to database . . . ";
+    assert_true(read_line() == "Attempting to connect to database...");
+    assert_true(read_line() == "Connected successfully");
+    assert_true(read_line() == "Loading classes...");
+    assert_true(read_line() == "Classes loaded");
+    assert_true(read_line() == "");
+
+    next_test("done");
+    printf("%i tests passed.\n", num_passed);
+    printf("%i tests failed.\n", num_failed);
     return 0;
 }
 
