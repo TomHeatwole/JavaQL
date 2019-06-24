23bbe959b40ff19fc65e7ba5d059dc9c1f8134de
tomheatwole <tgh8xn@virginia.edu>
2019-06-24 19:10:34 -0400

    Remove unnecessary install script

--- install-git-hook.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# ref: https://sigmoidal.io/automatic-code-quality-checks-with-git-hooks/
-
-GIT_DIR=$(git rev-parse --git-dir)
-
-echo "Installing hooks..."
-ln -s pre-commit.sh $GIT_DIR/hooks/pre-commit
-echo "Done!"
